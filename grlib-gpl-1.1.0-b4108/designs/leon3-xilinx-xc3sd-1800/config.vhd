9275e5d70c85e8844ce9c40144cc37c055e482d7
Yichen Dong <m15658011819@126.com>
2022-10-27 19:40:00 +0100

    top level

--- grlib-gpl-1.1.0-b4108/designs/leon3-xilinx-xc3sd-1800/config.vhd
+++ grlib-gpl-1.1.0-b4108/designs/leon3-xilinx-xc3sd-1800/config.vhd
@@ -28,7 +28,8 @@ package config is
   constant CFG_PCISYSCLK: integer := 0;
   constant CFG_CLK_NOFB : integer := 0;
 -- LEON3 processor core
-  constant CFG_LEON3 : integer := 1;
+  constant CFG_LEON3 : integer := 0;
+  constant CFG_CM0 : integer := 1;
   constant CFG_NCPU : integer := (1);
   constant CFG_NWIN : integer := (8);
   constant CFG_V8 : integer := 0; 
